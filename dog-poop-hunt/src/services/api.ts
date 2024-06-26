5e47e16639f2765793fe41f8d51d4c9684913fdd
Justin Lewis <justinmichlewis@gmail.com>
2024-06-25 17:01:17 -0700

    global function for api calls

---
+++ dog-poop-hunt/src/services/api.ts
@@ -0,0 +1,34 @@
+const deletePoop = async (poopId: string, pickedUserId: string) => {
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().split("T")[0];
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    state: "completed",
+    completedDate: formattedDate,
+    pickedUserId: pickedUserId,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  return await fetch(
+    "http://127.0.0.1:5000/api/poops/" + poopId,
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+      return result;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
+export { deletePoop };
