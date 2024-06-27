dea8377343c539322f7440f2a1845bf86a5d1ee7
Justin Lewis <justinmichlewis@gmail.com>
2024-06-27 15:23:11 -0700

    removed unneeded state

--- dog-poop-hunt/src/stores/user.ts
+++ dog-poop-hunt/src/stores/user.ts
@@ -6,9 +6,6 @@ export const useUserStore = defineStore("user", {
     firstName: useStorage("firstName", ""),
     lastName: useStorage("lastName", ""),
     email: useStorage("email", ""),
-    poopsCollected: useStorage("poopsColleded", 0),
-    poopsMissed: useStorage("poopsMissed", 0),
-    score: useStorage("score", 0),
     id: useStorage("id", 0),
   }),
   actions: {
@@ -16,26 +13,17 @@ export const useUserStore = defineStore("user", {
       firstName: string,
       lastName: string,
       email: string,
-      poopsCollected: number,
-      poopsMissed: number,
-      score: number,
       id: number
     ) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
-      this.poopsCollected = poopsCollected;
-      this.poopsMissed = poopsMissed;
-      this.score = score;
       this.id = id;
     },
     clearUserData() {
       this.firstName = "";
       this.lastName = "";
       this.email = "";
-      this.poopsCollected = 0;
-      this.poopsMissed = 0;
-      this.score = 0;
       this.id = 0;
     },
   },
