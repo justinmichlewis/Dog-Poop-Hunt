d417baaed2b9673588b204abf120c3cf8a4ff0ef
Justin Lewis <justinmichlewis@gmail.com>
2024-06-29 14:29:31 -0700

    chore: Update user store to include latitude and longitude storage

--- dog-poop-hunt/src/stores/user.ts
+++ dog-poop-hunt/src/stores/user.ts
@@ -7,24 +7,32 @@ export const useUserStore = defineStore("user", {
     lastName: useStorage("lastName", ""),
     email: useStorage("email", ""),
     id: useStorage("id", 0),
+    lat: useStorage("lat", 0),
+    lon: useStorage("lon", 0),
   }),
   actions: {
     setUserData(
       firstName: string,
       lastName: string,
       email: string,
-      id: number
+      id: number,
+      lat: number,
+      lon: number
     ) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
       this.id = id;
+      this.lat = lat;
+      this.lon = lon;
     },
     clearUserData() {
       this.firstName = "";
       this.lastName = "";
       this.email = "";
       this.id = 0;
+      this.lat = 0;
+      this.lon = 0;
     },
   },
 });
