08e819145115e153660194a9b44a28b826c8816b
Justin Lewis <justinmichlewis@gmail.com>
2024-06-29 14:29:56 -0700

    feat: Update me view to display achievements and add logout action bar
    The me view has been updated to display the achievements of the user, including the total number of poops collected and the total score. Additionally, the logout functionality has been moved to a separate action bar component.

--- dog-poop-hunt/src/views/Me.vue
+++ dog-poop-hunt/src/views/Me.vue
@@ -1,27 +1,39 @@
 <template>
-  <BaseHeader title="Settings" />
+  <BaseHeader title="About Me" />
   <div class="background">
     <h1>Dog Poop Hunt</h1>
     <h2>Welcome {{ userStore.firstName }}!</h2>
     <div class="flex">
       <div class="section">
-        You have {{ userStore.poopsCollected }} poops collected
+        You have {{ achievements.totalPoopsPicked }} poops collected
       </div>
-      <div class="section">You have a total score of {{ userStore.score }}</div>
+      <div class="section">
+        You have a total score of {{ achievements.totalPoints }}
+      </div>
     </div>
-    <h1>{{ userStore.id }}</h1>
-    <button @click="logOut">Log Out</button>
   </div>
-  <BaseNavBar :fixed="true" />
+  <div class="container-content">
+    <BaseActionBar bg-color="#84C877" @click="logOut"> Log Out </BaseActionBar>
+    <BaseNavBar />
+  </div>
 </template>
 <script setup lang="ts">
 import BaseNavBar from "@/components/BaseNavBar.vue";
 import BaseHeader from "@/components/BaseHeader.vue";
+import BaseActionBar from "@/components/BaseActionBar.vue";
 import { useUserStore } from "@/stores/user";
 import { useRouter } from "vue-router";
+import { ref, onMounted } from "vue";
+import { getAchievements } from "@/services/api";
 
 const userStore = useUserStore();
 const router = useRouter();
+let achievements = ref({ totalPoopsPicked: 0, totalPoints: 0 });
+
+onMounted(async () => {
+  achievements.value = await getAchievements(userStore.id);
+  console.log(achievements.value);
+});
 
 const logOut = () => {
   userStore.clearUserData();
@@ -53,5 +65,15 @@ button {
   font-size: larger;
   padding: 5px;
   margin: 10px;
+}
+.container-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  z-index: 9999;
 }
 </style>
